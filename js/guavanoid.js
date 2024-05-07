1c24c041263dc2a908056a531e2adb4889e9fc53
sanmaksim <sanmaksimovic@gmail.com>
2024-05-07 12:15:04 +1000

    isolated player class to separate file

--- js/guavanoid.js
+++ js/guavanoid.js
@@ -45,52 +45,6 @@ var Guavanoid = function() {
     // for time based animation
     var delta, then;
 
-    class Player {
-        x = '';
-        y = '';
-        width = '';
-        height = '';
-        color = '';
-        lives = 3;
-
-        constructor(x, y, width, height, color) {
-            this.x = x;
-            this.y = y;
-            this.width = width;
-            this.height = height;
-            this.color = color;
-        }
-
-        draw(ctx) {
-            // GOOD practice: save the context, use 2D trasnformations
-            ctx.save();
-
-            // translate the coordinate system, draw relative to it
-            ctx.translate(this.x, this.y);
-
-            // draw the player with its current color
-            ctx.fillStyle = this.color;
-
-            // draw the player at its current position, width and height
-            ctx.fillRect(0, 0, this.width, this.height);
-
-            // GOOD practice: restore the context
-            ctx.restore();
-        }
-
-        move(x) {
-            const halfPlayerWidth = this.width / 2;
-
-            if (x < halfPlayerWidth) {
-                this.x = x;
-            } else if (x > (w - halfPlayerWidth)) {
-                this.x = x - this.width;
-            } else {
-                this.x = x - halfPlayerWidth;
-            }
-        }
-    }
-
     class Block {
         x = '';
         y = '';
@@ -240,7 +194,7 @@ var Guavanoid = function() {
                 // test if the mouse is positioned over the canvas first
                 if(mousePos !== undefined) {
                     //player.move(mousePos.x, mousePos.y);
-                    player.move(mousePos.x);
+                    player.move(mousePos.x, w);
                     if (ball.isAttached) {
                         ball.followPlayer(mousePos.x, player, w);
                     }
