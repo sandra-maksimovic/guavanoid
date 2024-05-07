187cc584c178de22f3495abb4f67fd4de24aa4f1
sanmaksim <sanmaksimovic@gmail.com>
2024-05-07 12:17:05 +1000

    isolated block class to separate file

---
+++ js/block.js
@@ -0,0 +1,32 @@
+class Block {
+    x = '';
+    y = '';
+    width = '';
+    height = '';
+    color = '';
+
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw(ctx) {
+        // GOOD practice: save the context, use 2D trasnformations
+        ctx.save();
+
+        // translate the coordinate system, draw relative to it
+        ctx.translate(this.x, this.y);
+
+        // draw the player with its current color
+        ctx.fillStyle = this.color;
+
+        // draw the player at its current position, width and height
+        ctx.fillRect(0, 0, this.width, this.height);
+
+        // GOOD practice: restore the context
+        ctx.restore();
+    }
+}
--- js/guavanoid.js
+++ js/guavanoid.js
@@ -44,40 +44,7 @@ var Guavanoid = function() {
 
     // for time based animation
     var delta, then;
-
-    class Block {
-        x = '';
-        y = '';
-        width = '';
-        height = '';
-        color = '';
-
-        constructor(x, y, width, height, color) {
-            this.x = x;
-            this.y = y;
-            this.width = width;
-            this.height = height;
-            this.color = color;
-        }
-
-        draw(ctx) {
-            // GOOD practice: save the context, use 2D trasnformations
-            ctx.save();
-
-            // translate the coordinate system, draw relative to it
-            ctx.translate(this.x, this.y);
-
-            // draw the player with its current color
-            ctx.fillStyle = this.color;
-
-            // draw the player at its current position, width and height
-            ctx.fillRect(0, 0, this.width, this.height);
-
-            // GOOD practice: restore the context
-            ctx.restore();
-        }
-    }
-
+    
     var loadAssets = function(callback) {
         // load embedded sounds
         blockCollisionSound = new Howl({
